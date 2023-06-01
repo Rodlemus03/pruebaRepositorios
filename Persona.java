36e1eea72524d78202cf39094d61c43faf4c41dd
Diegomorales21 <135185055+Diegomorales21@users.noreply.github.com>
2023-05-31 19:24:04 -0600

    Prueba commit

---
+++ Persona.java
@@ -0,0 +1,13 @@
+public class Persona {
+    private int edad;
+    private String nombre;
+    private String apellidos;
+    private String profesion;
+    public Persona(int edad, String nombre, String apellidos, String profesion){
+        this.edad=edad;
+        this.nombre=nombre;
+        this.apellidos=apellidos;
+        this.profesion=profesion;
+    }
+
+}
