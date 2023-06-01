4732fef3e6917c72310701604e079ce74ecdb169
Mauricio Julio Rodrigo Lemus Guzmán <rodlemus03@gmail.com>
2023-05-31 19:29:50 -0600

    Crear metodos

--- Menu.java
+++ Menu.java
@@ -1,3 +1,6 @@
 public class Menu {
-    
+    public static void main(String[] args) {
+        Persona p1= new Persona(18,"Julio","Lemus","Estudiante");
+        System.out.println(p1);
+    }
 }
--- Persona.java
+++ Persona.java
@@ -10,4 +10,48 @@ public class Persona {
         this.profesion=profesion;
     }
 
+
+    @Override
+    public String toString() {
+        return "{" +
+            " edad='" + getEdad() + "'" +
+            ", nombre='" + getNombre() + "'" +
+            ", apellidos='" + getApellidos() + "'" +
+            ", profesion='" + getProfesion() + "'" +
+            "}";
+    }
+
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return this.apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getProfesion() {
+        return this.profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+
 }
